6af4be696b2fbb7720b0288d76c109f1e6041af1
Ilya <95875204+luiofff@users.noreply.github.com>
2024-05-03 14:30:36 -0700

    fix bugs

--- calendar/index.js
+++ calendar/index.js
@@ -1,8 +1,10 @@
-document.querySelector('.calendar-cardBlock').addEventListener('click', function() {
-    let menuBlock = this.querySelector('.calendar-card__MenuBlock');
-    if (menuBlock.style.display === 'none') {
-        menuBlock.style.display = 'grid';
-    } else {
-        menuBlock.style.display = 'none';
-    }
+document.querySelectorAll('.calendar-cardBlock').forEach(function(element) {
+    element.addEventListener('click', function() {
+        let menuBlock = this.querySelector('.calendar-card__MenuBlock');
+        if (menuBlock.style.display === 'none') {
+            menuBlock.style.display = 'grid';
+        } else {
+            menuBlock.style.display = 'none';
+        }
+    });
 });
